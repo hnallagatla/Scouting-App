be3ee8c9a62c168822e6aabe5224d7572210dcf8
hnallagatla <144624450+hnallagatla@users.noreply.github.com>
2024-02-11 18:02:48 -0500

    FiNaLlY wOrKeDdDd :)))))

--- user.js
+++ user.js
@@ -70,11 +70,12 @@ function generateQRCode() {
     for (i = 0; i < questions.length; i++) {qrCodeString += getInputValue(questions[i]) + "|";}
 
     var qrArray = qrCodeString.split("|");
-    qrArray.splice(2, 0, inAllianceString)
-    qrArray.splice(3, 0, whereInCommunityString)
-    qrArray.splice(4, 0, leftCommunityString)
+    qrArray.splice(2, 0, inAllianceString);
+    qrArray.splice(3, 0, whereInCommunityString);
+    qrArray.splice(4, 0, leftCommunityString);
+    qrArray.splice(7,0,autoScore);
 
-    qrCodeString = qrArray.join("|")
+    qrCodeString = qrArray.join("|");
 
     document.getElementById("qrcode").innerHTML = "";
     new QRCode(document.getElementById("qrcode"), qrCodeString);
