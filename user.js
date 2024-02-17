2a9d98d8c45115f5621f25759442cc2d1e5303c2
hnallagatla <144624450+hnallagatla@users.noreply.github.com>
2024-02-17 17:32:28 -0500

    Added clear button to erase all fields

--- user.js
+++ user.js
@@ -9,7 +9,7 @@ var highlightedButtons = {};
 const questions = ["team", "match", "speaker", "amp", "ranking"];
 
 //Get value of clicked buttons from html and assign similar values to variables - leftCommunity() and inAlliance()
-//The call to updateTotalScore() in leftCommunity(), addition(), and subtraction is needed to calculate total auto score correctly
+//The call to updateTotalScore() at the end of most of these methods is needed to calculate total auto score correctly
 //Used ternary operators for leftCommunity() and inAlliance() instead of using if blocks. Both have same functionality but ternary looks cleaner
 //Only use ternary when there are two possible vals. This means when there are 3 or more buttons in one section on the website, it will be hard to use ternary
 
@@ -32,6 +32,23 @@ function subtraction(element) {
     updateTotalScore();
 }
 
+//Goes through questions array and sets all values back to zero. Calls updateTotalScore() to change total score field on website to 0. 
+function clearForm(){
+    for (i = 0; i < questions.length; i++) {document.getElementById(questions[i] + "input").value = 0;}
+    updateTotalScore();
+}
+
+//Unhighlights buttons. IMPORTANT: Does not change whether or not button has been actually clicked. Only changes Frontend(CSS)
+//Sets leftCommunityPoints to zero and calls updateTotalScore() to reset points for leaving community back to 0. 
+function unhighlightButtons(){
+    var buttons = document.querySelectorAll('.general-button, .num-button');
+    buttons.forEach(function(button) {
+        button.classList.remove('highlighted');
+    });
+    leftCommunityPoints = 0;
+    updateTotalScore();
+}
+
 //Changes the total score of auto dynamically on the website as user presses buttons
 function updateTotalScore() {
     var speakerScore = parseInt(document.getElementById("speakerinput").value) * 5;
@@ -48,7 +65,7 @@ function highlightButton(sectionId, button) {
     if (currentHighlightedButton) {currentHighlightedButton.classList.remove('highlighted');}
     button.classList.add('highlighted');
     highlightedButtons[sectionId] = button;
-}
+}   
 
 function getInputValue(element) {
     var inputVal = document.getElementById(element + "input").value;
